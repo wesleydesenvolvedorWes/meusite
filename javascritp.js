9f99bba3171a19d9ab2f66a6705e437388ef81dd
WESLEY B DOS SANTOS <71525476+wesleydesenvolvedorWes@users.noreply.github.com>
2020-10-13 13:11:47 -0300

    javascritp.js

---
+++ javascritp.js
@@ -0,0 +1,104 @@
+
+// Exemplo de partida JavaScript para desativar envios de formulários se houver campos inválidos
+(function () {
+    'use strict'
+  
+    window.addEventListener('load', function () {
+      // Buscar todos os formulários que queremos aplicar estilos de validação bootstrap personalizados para
+      var forms = document.getElementsByClassName('needs-validation')
+  
+      // Loop sobre eles e impedir a submissão
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+    }, false)
+  }())
+
+
+$(function () {
+    'use strict'
+  
+    $('[data-toggle="offcanvas"]').on('click', function () {
+      $('.offcanvas-collapse').toggleClass('open')
+    })
+  })
+  
+  (function () {
+    var options = {
+        facebook: "100036754243412", // Facebook page ID
+        whatsapp: "+5511988574097", // WhatsApp number
+        call_to_action: "Falar com Wesley", // Call to action
+        button_color: "#129BF4", // Color of button
+        position: "right", // Position may be 'right' or 'left'
+        order: "whatsapp,facebook", // Order of buttons
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+  })();
+
+
+  function Enviar(){
+    valida =Enviar();
+    if(valida != ""){
+      alert(valida);
+      return;
+    }
+    
+    data = {  nome: $("#Nome").val(),
+              sobrenome: $("#Sobrenome").val(),
+              destino: $("#Destino").val(),
+              ordem: $("#Ordem").val(),
+            };
+     $.post("formulario.asp?acao=Enviar", data, function (retorno) {
+        if (retorno == "ok"){
+          MontaListaDestinatario();
+          alert("usuário inserido com sucesso.");
+           alert("Pressione em MostrarTodos.");
+           data = {  tipo: $("#tipo").val(''),
+              email: $("#Email").val(''),
+              destino: $("#Destino").val(''),
+              ordem: $("#Ordem").val(''),
+    
+    
+            };
+        }else{
+          alert("Falha na inclusão do usuário!");
+        }
+     }); 
+    data = {  tipo: $("#tipo").val(''),
+              banco_dados: $("#BancoDeDados").val(''),
+              frequencia: $("#FrequenciaDeEnvio").val(''),
+              titulo: $("#Nome").val(""),
+              procedure_boletim: $("#ProcedureDeEnvio").val(''),
+              servidor: $("#ServidorDeEnvio").val(''),
+            };
+    
+        }
+    function Validarformulario(){
+    var msg = "";
+    
+    if($("#Nome").val() == ""){
+      msg += "Digite um nome válido.\n";
+    }
+    if($("#Email").val() == ""){
+      msg += "Digite um e-mail válido.\n";
+    }
+    if($("#Destino").val() == ""){
+      msg += "Selecione o destino.\n";
+    }
+    if($("#Ordem").val() == ""){
+      msg += "Digite a ordem.\n";
+    
+    }
+    
+    return msg;
+    }
+     
