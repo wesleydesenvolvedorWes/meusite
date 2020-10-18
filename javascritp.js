9de705e98b3e7b847c24bb2914d0fb3c1e061943
WESLEY B DOS SANTOS <71525476+wesleydesenvolvedorWes@users.noreply.github.com>
2020-10-18 18:21:05 -0300

    Add files via upload

--- javascritp.js
+++ javascritp.js
@@ -108,9 +108,8 @@ $(function () {
       $('.time').mask('00:00:00');
       $('.date_time').mask('00/00/0000 00:00:00');
       $('.cep').mask('00000-000');
-      $('.phone_with_ddd').mask('(00) 0000-0000');
-      $('.phone_with_ddd').mask('(00) 0 0000-0000');
-      $('.phone_us').mask('(000) 000-0000');
+      $('.tel').mask('(00)0000-0000');
+      $('.tel').mask('(00)00000-0000');
       $('.mixed').mask('AAA 000-S0S');
       $('.cpf').mask('000.000.000-00', {reverse: true});
       $('.money').mask('000.000.000.000.000,00', {reverse: true});
